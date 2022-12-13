2d7bc0699c5830a805f43e88bbb43d3baefaf1c5
Dawid Antosz <64035334+DawidAntosz@users.noreply.github.com>
2022-12-13 09:19:08 +0100

    Update i2c_communication.py

--- Program/i2c_communication.py
+++ Program/i2c_communication.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python
-#############################
+# -*- coding: utf-8 -*-
 import smbus
 import time
-#############################
-
 
 bus = smbus.SMBus(1)
 
 address = 0x04
 
 def writeNumeber(value):
-
 	bus.write_byte(address, value)
-
 	return -1
 
 def readNumber():
-
 	number = bus.read_byte_data(address, 1)
-
 	return number
 
-while True:
 
+def main():
+	
+    while True:
 	data = raw_input("Enter the data to be sent :")
 	data_list = list(data)
 	for i in data_list:
 		writeNumber(int(ord(i)))
 		time.sleep(.1)
 	writeNumber(int(0x0A))
+
+	
+if __name__ == '__main__':
+    main()
