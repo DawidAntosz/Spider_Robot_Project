e871a3ad5145d4b756367bdf950abc92b4c888a5
Dawid Antosz <64035334+DawidAntosz@users.noreply.github.com>
2022-12-13 09:20:51 +0100

    Update Pajak_Mechatroniczny.ino

--- Program/Pajak_Mechatroniczny.ino
+++ Program/Pajak_Mechatroniczny.ino
@@ -1,5 +1,3 @@
-///////VARIABLES//////
-
 #include <FlexiTimer2.h>
 #include <Servo.h>   
 #include <Wire.h>
@@ -11,7 +9,6 @@
 
 Servo servo[4][3];
 const int servo_pin[4][3] = { {2, 3, 4}, {5, 6, 7}, {8, 9, 10}, {11, 12, 13} };
-
 
 int wart        ;
 int przod  = 14 ;
